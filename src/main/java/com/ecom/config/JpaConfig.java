3daba04208290586f741a8bfba2a6e5d90ad26a9
UZABUMWANA Fiston <youfistalline11@gmail.com>
2025-05-23 03:24:24 +0200

    link update 4

---
+++ src/main/java/com/ecom/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.ecom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false); // Let Hibernate handle DDL
+        return adapter;
+    }
+}
